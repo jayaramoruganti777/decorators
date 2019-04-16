feec60c268ba34d5d5d9a06dcf235eb9bd974e7a
jayaramoruganti777 <42366285+jayaramoruganti777@users.noreply.github.com>
2019-04-16 18:37:37 +0530

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,10 @@
+def hello():
+    print('hello im inside exp1')
+    
+def helloindirectly():
+    print('hello indirect')
+    
+if __name__ == '__main__':
+    hello()
+else:
+    helloindirectly()
---
+++ exp2.py
@@ -0,0 +1,4 @@
+
+import exp1
+
+print('i am in exp2.py ')
